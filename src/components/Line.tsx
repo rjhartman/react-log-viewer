688645f4d1add5ea883da8ae594f55aa0f59f0b6
Tom Hartman <94thartman@gmail.com>
2023-02-18 18:52:07 -0500

    there happy now?

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -12,6 +12,13 @@ interface DivProps {
   selected: boolean
 }
 
+interface LineNoProps {
+  selected: boolean
+  width: number
+}
+
+const CHAR_WIDTH = 0.6
+
 const Div = styled.div<DivProps>`
   font-family: 'Roboto', 'Courier';
   font-style: monospace;
@@ -30,12 +37,18 @@ const Div = styled.div<DivProps>`
   padding: 0 1rem;
 `
 
+const LineNo = styled.div<LineNoProps>`
+  display: flex;
+  justify-content: flex-end;
+  min-width: ${(p) => p.width * CHAR_WIDTH}rem;
+  color: ${(p) => (p.selected ? '#fff' : '#888')};
+  margin-right: 1rem;
+  text-align: right;
+`
+
 const Line: React.FC<ListChildComponentProps> = ({ data, index, style }) => {
   const { showLineNumbers } = useSettingsContext()
   const [selected, setSelected] = useState(false)
-
-  let line = data[index]
-  if (showLineNumbers) line = `${index + 1} ${line}`
 
   return (
     <Div
@@ -45,7 +58,12 @@ const Line: React.FC<ListChildComponentProps> = ({ data, index, style }) => {
       }}
       style={style}
     >
-      {line}
+      {showLineNumbers && (
+        <LineNo selected={selected} width={(data.length + 1).toString().length}>
+          {index + 1}|
+        </LineNo>
+      )}
+      {data[index]}
     </Div>
   )
 }
